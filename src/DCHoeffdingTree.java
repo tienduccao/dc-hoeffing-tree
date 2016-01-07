91e73a9e89cc035b8380d746fb20411dc7b89632
tienduccao <duccao1989@gmail.com>
2016-01-07 21:14:46 +0100

    Create the root node

--- src/DCHoeffdingTree.java
+++ src/DCHoeffdingTree.java
@@ -1,21 +1,28 @@
 import moa.classifiers.AbstractClassifier;
 import moa.core.Measurement;
+import weka.core.Attribute;
 import weka.core.Instance;
 
 /**
  * Created by duccao on 07/01/16.
  */
 public class DCHoeffdingTree extends AbstractClassifier {
+    private Node root;
+
     /************************************************************
      *** Methods from AbstractClassifier
      ************************************************************/
     @Override
     public void resetLearningImpl() {
-        // create the root node
     }
 
     @Override
     public void trainOnInstanceImpl(Instance instance) {
+        // create the root node if necessary
+        if (root == null) {
+            root = createRootNode(instance);
+        }
+
         // find the leaf node and update sufficient stats
 
         // check whether we have to split
@@ -44,4 +51,14 @@ public class DCHoeffdingTree extends AbstractClassifier {
     /************************************************************
      *** Implementation of HoeffdingTree
      ************************************************************/
+    private Node createRootNode(Instance instance) {
+        int numAttributes = instance.numAttributes();
+        Attribute[] attributes = new Attribute[numAttributes];
+
+        for (int i = 0; i < numAttributes; ++i) {
+            attributes[i] = instance.attribute(i);
+        }
+
+        return new Node(attributes, instance.classAttribute());
+    }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,12 @@
+import weka.core.Attribute;
+
 /**
  * Created by duccao on 07/01/16.
  */
 public class Node {
+    private Attribute classAttribute;
+
+    public Node(Attribute[] attributes, Attribute classAttribute) {
+        this.classAttribute = classAttribute;
+    }
 }
