6dcc761929576e09e6d22cc5125e0be1aaab8855
tienduccao <duccao1989@gmail.com>
2016-01-08 11:00:32 +0100

    Init class InformationGain

---
+++ src/InformationGain.java
@@ -0,0 +1,11 @@
+import weka.core.Attribute;
+
+/**
+ * Created by duccao on 08/01/16.
+ */
+public class InformationGain implements SplitFunction {
+    @Override
+    public double value(Node node, Attribute attribute) {
+        return 0;
+    }
+}
