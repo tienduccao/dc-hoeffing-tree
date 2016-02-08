456cccafea197ea0b426a6974ad97c4bfc17f7c9
tienduccao <duccao1989@gmail.com>
2016-02-08 10:40:13 +0100

    Init methods of AbstractClassifier

--- src/DCHoeffdingTree.java
+++ src/DCHoeffdingTree.java
@@ -1,5 +1,41 @@
+import moa.classifiers.AbstractClassifier;
+import moa.core.Measurement;
+import weka.core.Instance;
+
 /**
  * Created by duccao on 08/02/16.
  */
-public class DCHoeffdingTree {
+public class DCHoeffdingTree extends AbstractClassifier {
+    /************************************************************
+     *** Methods from AbstractClassifier
+     ************************************************************/
+    @Override
+    public void resetLearningImpl() {
+
+    }
+
+    @Override
+    public void trainOnInstanceImpl(Instance instance) {
+
+    }
+
+    @Override
+    protected Measurement[] getModelMeasurementsImpl() {
+        return new Measurement[0];
+    }
+
+    @Override
+    public void getModelDescription(StringBuilder stringBuilder, int i) {
+
+    }
+
+    @Override
+    public boolean isRandomizable() {
+        return false;
+    }
+
+    @Override
+    public double[] getVotesForInstance(Instance instance) {
+        return new double[0];
+    }
 }
