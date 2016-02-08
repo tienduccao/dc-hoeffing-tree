725ba988fbef3f0f8f0c5c73fceef21fe2678937
tienduccao <duccao1989@gmail.com>
2016-02-08 16:03:11 +0100

    Update docs for attemptToSplit

--- src/tree/DCHoeffdingTree.java
+++ src/tree/DCHoeffdingTree.java
@@ -9,7 +9,6 @@ import moa.classifiers.core.AttributeSplitSuggestion;
 import moa.classifiers.core.attributeclassobservers.AttributeClassObserver;
 import moa.classifiers.core.attributeclassobservers.DiscreteAttributeClassObserver;
 import moa.classifiers.core.attributeclassobservers.NumericAttributeClassObserver;
-import moa.classifiers.core.conditionaltests.InstanceConditionalTest;
 import moa.classifiers.core.splitcriteria.SplitCriterion;
 import moa.core.Measurement;
 import moa.options.ClassOption;
@@ -184,80 +183,60 @@ public class DCHoeffdingTree extends AbstractClassifier {
         return (AttributeClassObserver) numericClassObserver.copy();
     }
 
-    protected void attemptToSplit(ActiveLearningNode node, SplitNode parent,
-                                  int parentIndex) {
+    protected void attemptToSplit(ActiveLearningNode node, SplitNode parent, int parentBranch) {
         if (!node.observedClassDistributionIsPure()) {
+            // get list of split suggestions and sort them
             SplitCriterion splitCriterion = (SplitCriterion) getPreparedClassOption(this.splitCriterionOption);
             AttributeSplitSuggestion[] bestSplitSuggestions = node.getBestSplitSuggestions(splitCriterion, this);
             Arrays.sort(bestSplitSuggestions);
+
+            // determine whether we should split based on Hoeffding bound or tie threshold
             boolean shouldSplit = false;
-            if (bestSplitSuggestions.length < 2) {
-                shouldSplit = bestSplitSuggestions.length > 0;
-            } else {
-                double hoeffdingBound = computeHoeffdingBound(splitCriterion.getRangeOfMerit(node.getObservedClassDistribution()),
-                        this.splitConfidenceOption.getValue(), node.getWeightSeen());
+            if (bestSplitSuggestions.length > 2) {
+                double hoeffdingBound = computeHoeffdingBound(
+                        splitCriterion.getRangeOfMerit(node.getObservedClassDistribution()),
+                        this.splitConfidenceOption.getValue(),
+                        node.getWeightSeen()
+                );
                 AttributeSplitSuggestion bestSuggestion = bestSplitSuggestions[bestSplitSuggestions.length - 1];
                 AttributeSplitSuggestion secondBestSuggestion = bestSplitSuggestions[bestSplitSuggestions.length - 2];
-                if ((bestSuggestion.merit - secondBestSuggestion.merit > hoeffdingBound)
-                        || (hoeffdingBound < this.tieThresholdOption.getValue())) {
-                    shouldSplit = true;
-                }
-                // }
+                shouldSplit =
+                        bestSuggestion.merit - secondBestSuggestion.merit > hoeffdingBound ||
+                        hoeffdingBound < this.tieThresholdOption.getValue();
 
                 // TODO remove poor attributes
-//                if ((this.removePoorAttsOption != null)
-//                        && this.removePoorAttsOption.isSet()) {
-//                    Set<Integer> poorAtts = new HashSet<Integer>();
-//                    // scan 1 - add any poor to set
-//                    for (int i = 0; i < bestSplitSuggestions.length; i++) {
-//                        if (bestSplitSuggestions[i].splitTest != null) {
-//                            int[] splitAtts = bestSplitSuggestions[i].splitTest.getAttsTestDependsOn();
-//                            if (splitAtts.length == 1) {
-//                                if (bestSuggestion.merit
-//                                        - bestSplitSuggestions[i].merit > hoeffdingBound) {
-//                                    poorAtts.add(new Integer(splitAtts[0]));
-//                                }
-//                            }
-//                        }
-//                    }
-//                    // scan 2 - remove good ones from set
-//                    for (int i = 0; i < bestSplitSuggestions.length; i++) {
-//                        if (bestSplitSuggestions[i].splitTest != null) {
-//                            int[] splitAtts = bestSplitSuggestions[i].splitTest.getAttsTestDependsOn();
-//                            if (splitAtts.length == 1) {
-//                                if (bestSuggestion.merit
-//                                        - bestSplitSuggestions[i].merit < hoeffdingBound) {
-//                                    poorAtts.remove(new Integer(splitAtts[0]));
-//                                }
-//                            }
-//                        }
-//                    }
-//                    for (int poorAtt : poorAtts) {
-//                        node.disableAttribute(poorAtt);
-//                    }
-//                }
             }
+
+            // split
             if (shouldSplit) {
                 AttributeSplitSuggestion splitDecision = bestSplitSuggestions[bestSplitSuggestions.length - 1];
                 if (splitDecision.splitTest == null) {
+                    // TODO what is this?
                     // preprune - null wins
-                    deactivateLearningNode(node, parent, parentIndex);
+                    deactivateLearningNode(node, parent, parentBranch);
                 } else {
-                    SplitNode newSplit = newSplitNode(splitDecision.splitTest,
-                            node.getObservedClassDistribution(),splitDecision.numSplits() );
+                    SplitNode newSplit = new SplitNode(
+                            splitDecision.splitTest,
+                            node.getObservedClassDistribution(),
+                            splitDecision.numSplits()
+                    );
+
                     for (int i = 0; i < splitDecision.numSplits(); i++) {
                         Node newChild = newLearningNode(splitDecision.resultingClassDistributionFromSplit(i));
                         newSplit.setChild(i, newChild);
                     }
+
                     this.activeLeafNodeCount--;
                     this.decisionNodeCount++;
                     this.activeLeafNodeCount += splitDecision.numSplits();
+
                     if (parent == null) {
                         this.treeRoot = newSplit;
                     } else {
-                        parent.setChild(parentIndex, newSplit);
+                        parent.setChild(parentBranch, newSplit);
                     }
                 }
+
                 // TODO manage memory
 //                enforceTrackerLimit();
             }
@@ -274,11 +253,6 @@ public class DCHoeffdingTree extends AbstractClassifier {
         }
         this.activeLeafNodeCount--;
         this.inactiveLeafNodeCount++;
-    }
-
-    protected SplitNode newSplitNode(InstanceConditionalTest splitTest,
-                                     double[] classObservations, int size) {
-        return new SplitNode(splitTest, classObservations, size);
     }
 
     public static double computeHoeffdingBound(double range, double confidence, double n) {
