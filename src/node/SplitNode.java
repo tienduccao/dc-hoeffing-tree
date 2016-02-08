2fbe82c2715a964bea7a2b4d893b013ca344d7c2
tienduccao <duccao1989@gmail.com>
2016-02-08 13:58:12 +0100

    Remove SplitNode.instanceChildIndex

--- src/node/SplitNode.java
+++ src/node/SplitNode.java
@@ -13,13 +13,13 @@ public class SplitNode extends Node {
 
     protected InstanceConditionalTest splitTest;
 
-    protected AutoExpandVector<Node> children; // = new AutoExpandVector<Node>();
+    protected AutoExpandVector<Node> children;
 
     public SplitNode(InstanceConditionalTest splitTest,
                      double[] classObservations, int size) {
         super(classObservations);
         this.splitTest = splitTest;
-        this.children = new AutoExpandVector<Node>(size);
+        this.children = new AutoExpandVector<>(size);
     }
 
     public void setChild(int index, Node child) {
@@ -30,14 +30,9 @@ public class SplitNode extends Node {
         return this.children.get(index);
     }
 
-    public int instanceChildIndex(Instance inst) {
-        return this.splitTest.branchForInstance(inst);
-    }
-
     @Override
-    public FoundNode filterInstanceToLeaf(Instance inst, SplitNode parent,
-                                          int parentBranch) {
-        int childIndex = instanceChildIndex(inst);
+    public FoundNode filterInstanceToLeaf(Instance inst, SplitNode parent, int parentBranch) {
+        int childIndex = this.splitTest.branchForInstance(inst);
         if (childIndex >= 0) {
             Node child = getChild(childIndex);
             if (child != null) {
